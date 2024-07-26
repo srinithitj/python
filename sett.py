47883cc279e6e4a40dd3c3d41b0ded8e4956c3cf
srinithitj <128626432+srinithitj@users.noreply.github.com>
2024-07-26 08:27:58 +0530

    Add files via upload

---
+++ sett.py
@@ -0,0 +1,10 @@
+a={1,2,3,4,5}
+b={4,5,6,7,8}
+print("union of a&b: ",a|b)
+print("intersection of a&b: ",a&b)
+print("difference: ",a-b)
+print("is a sunbset of b?: ",a.issubset(b))
+print("is b sunbset of a?: ",b.issubset(a))
+print("is a superset of b?: ",a.issuperset(b))
+print("is b superset of a?: ",b.issuperset(a))
+
